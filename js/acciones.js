17e2a49162c16c95164781c57a7a2067bbdb35e2
AL09R14-2 <pedro270143@gmail.com>
2015-08-18 11:32:56 -0500

    botonsonido
    poner sonido al boton

--- js/acciones.js
+++ js/acciones.js
@@ -1,7 +1,26 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
+//document.addEventListener("deviceready",function(){
 	
-}); 
+$('#btn_sonido').click(function(){
+	//alert("click a btn_sonido");
+	if($('#btn_sonido').hasClass('ui-icon-audio'))
+	{
+		//apagar el audio
+		$('#btn_sonido').removeClass('ui-icon-audio');
+		$('#btn_sonido').addClass('ui-icon-bars');
+		alert('se apago el sonido')
+	}
+	else
+	{
+		//prender el audio
+		$('#btn_sonido').removeClass('ui-icon-bars');
+		$('#btn_sonido').addClass('ui-icon-audio');
+		navigator.notification.beep(1);
+		alert('se prendio el sonido')
+	}
+});//click a btn_sonido
+
+//}); 
 });
 
